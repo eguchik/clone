bc281a5adebb5f0ff7759c16f56fab3ad5480bca
kei_e <k.eguchi84@gmail.com>
2021-08-28 12:02:15 +0900

    socket ioの導入２

--- src/socketapi.js
+++ src/socketapi.js
@@ -3,9 +3,17 @@ const socketapi = {
   io: io,
 };
 
+const db = require("./models");
 // Add your socket.io logic here!
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
+    const newTweet = db.tweets.build({
+      user_id: msg.user_id,
+      tweet: msg.tweet,
+    });
+    newTweet.save();
+    console.log(msg.user_id, msg.tweet);
+
     io.emit("chat message", msg);
   });
 });
